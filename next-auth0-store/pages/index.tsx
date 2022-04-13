84f552ca97db5609c1abb1ca5e1778d8de5c1ff3
Rafael Marangoni Waterkemper <rafael.waterkemper@betha.com.br>
2022-04-13 14:00:31 -0300

    Create the guards to redirect the user to login once it's not logged in

--- next-auth0-store/pages/index.tsx
+++ next-auth0-store/pages/index.tsx
@@ -1,16 +1,26 @@
-import { useUser } from "@auth0/nextjs-auth0";
+import { getAccessToken, getSession, useUser } from "@auth0/nextjs-auth0";
+import { GetServerSideProps } from "next";
 
 export default function Home() {
-  const { user } = useUser();
-  return (
-    <>
-      <h1>Hello Next</h1>
-
-      <pre>
-        {JSON.stringify(user, null, 2)}
-      </pre>
-
-      <a href="/api/auth/login">Login</a>
-    </>
-  );
+  return null;
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      redirect: {
+        destination: "/app",
+        permanent: false,
+      },
+    };
+  }
+};
