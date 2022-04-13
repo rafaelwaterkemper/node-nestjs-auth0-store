a2d6f2c458055817afed857b8dde7010173fd306
Rafael Marangoni Waterkemper <rafael.waterkemper@betha.com.br>
2022-04-12 23:01:42 -0300

    Started the project and configured the auth0 API

---
+++ nestjs-auth0-purchases/src/http/auth/authorization.guard.ts
@@ -0,0 +1,42 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import jwt from 'express-jwt';
+import { expressJwtSecret } from 'jwks-rsa';
+import { promisify } from 'node:util';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  private AUTH0_AUDIENCE: string;
+  private AUTH0_DOMAIN: string;
+
+  constructor(private configService: ConfigService) {
+    this.AUTH0_AUDIENCE = this.configService.get('AUTH0_AUDIENCE') ?? '';
+    this.AUTH0_DOMAIN = this.configService.get('AUTH0_DOMAIN') ?? '';
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+    const res = context.switchToHttp().getResponse();
+
+    const jwtCheck = promisify(
+      jwt({
+        secret: expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: `${this.AUTH0_DOMAIN}.well-known/jwks.json`,
+        }),
+        audience: 'first-auth-api',
+        issuer: 'https://waterkemper.us.auth0.com/',
+        algorithms: ['RS256'],
+      }),
+    );
+
+    try {
+      await jwtCheck(req, res);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+}
